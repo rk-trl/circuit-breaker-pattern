c632d493900caedd1f93bb2915cc7ff3b7b9fb12
rk-trl <110337072+rk-trl@users.noreply.github.com>
2023-12-26 15:10:20 +0530

    Update Program.cs with retry logic commented one

--- CircuitBreaker/Program.cs
+++ CircuitBreaker/Program.cs
@@ -2,6 +2,16 @@ using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//retry from poly (apart from circuit breaker)
+//var retryPolicy = Policy.HandleResult<HttpResponseMssage>(r=> 1r.IsSuccessStatusCode)
+//    .OrTransientHttpError()
+//    .WaitAndRetryAsync(3, retryDuration => TimeSpan.FromSeconds(120));
+//builder.Services.AddHttpClient("proucctservice", c =>
+//{
+//    c.BaseAddress = new Uri("http://localhost:5050/");
+//}).AddPolicyHandler(retryPolicy);
+
+
 // Add services to the container.
 //It will try to call the mentioned uri, as its going to break. So the next upcoming request will be blocked for 30 seconds
 builder.Services.AddHttpClient("errorApiClient", c =>
